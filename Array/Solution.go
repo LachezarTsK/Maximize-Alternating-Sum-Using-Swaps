2ad3b4dda51069f4bc5443661f9139be774f91c6
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-11-22 18:33:03 +0100

    Create Solution.go

---
+++ Array/Solution.go
@@ -0,0 +1,97 @@
+
+package main
+import "slices"
+
+func maxAlternatingSum(input []int, swaps [][]int) int64 {
+    unionFind := NewUnionFind(len(input))
+    for _, swap := range swaps {
+        unionFind.joinByRank(swap[0], swap[1])
+    }
+
+    var parentToConnectedComponents []ConnectedComponents = createMapParentToConnectedComponents(input, &unionFind)
+
+    return calculateMaxAlternatingSum(parentToConnectedComponents)
+}
+
+func createMapParentToConnectedComponents(input []int, unionFind *UnionFind) []ConnectedComponents {
+    parentToConnectedComponents := make([]ConnectedComponents, len(input))
+    for i := range input {
+        parent := unionFind.findParent(i)
+        if len(parentToConnectedComponents[parent].values) == 0 {
+            parentToConnectedComponents[parent] = ConnectedComponents{}
+        }
+        if isEven(i) {
+            parentToConnectedComponents[parent].numberOfEvenIndexes++
+        }
+        parentToConnectedComponents[parent].values = append(parentToConnectedComponents[parent].values, input[i])
+    }
+
+    return parentToConnectedComponents
+}
+
+func calculateMaxAlternatingSum(parentToConnectedComponents []ConnectedComponents) int64 {
+    var maxAlternatingSum int64 = 0
+
+    for _, current := range parentToConnectedComponents {
+        if len(current.values) == 0 {
+            continue
+        }
+        slices.SortFunc(current.values, func(first int, second int) int { return second - first })
+        for i := 0; i < current.numberOfEvenIndexes; i++ {
+            maxAlternatingSum += int64(current.values[i])
+        }
+        for i := current.numberOfEvenIndexes; i < len(current.values); i++ {
+            maxAlternatingSum -= int64(current.values[i])
+        }
+    }
+    return maxAlternatingSum
+}
+
+func isEven(value int) bool {
+    return value % 2 == 0
+}
+
+type ConnectedComponents struct {
+    numberOfEvenIndexes int
+    values              []int
+}
+
+type UnionFind struct {
+    parent []int
+    rank   []int
+}
+
+func NewUnionFind(numberOfElements int) UnionFind {
+    unionFind := UnionFind{
+        parent: make([]int, numberOfElements),
+        rank:   make([]int, numberOfElements),
+    }
+    for i := range numberOfElements {
+        unionFind.parent[i] = i
+        unionFind.rank[i] = 1
+    }
+    return unionFind
+}
+
+func (this *UnionFind) findParent(index int) int {
+    if this.parent[index] != index {
+        this.parent[index] = this.findParent(this.parent[index])
+    }
+    return this.parent[index]
+}
+
+func (this *UnionFind) joinByRank(indexOne int, indexTwo int) {
+    first := this.findParent(indexOne)
+    second := this.findParent(indexTwo)
+    if first == second {
+        return
+    }
+
+    if this.rank[first] >= this.rank[second] {
+        this.parent[second] = first
+        this.rank[first] += this.rank[second]
+    } else {
+        this.parent[first] = second
+        this.rank[second] += this.rank[first]
+    }
+}
