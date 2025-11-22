ddba467d1408af615a684d8703ae83e9eb4d7d57
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-11-22 18:51:50 +0100

    Create Solution.ts

---
+++ Solutions with Hash Map/Solution.ts
@@ -0,0 +1,91 @@
+
+function maxAlternatingSum(input: number[], swaps: number[][]): number {
+
+    const unionFind = new UnionFind(input.length);
+    for (let swap of swaps) {
+        unionFind.joinByRank(swap[0], swap[1]);
+    }
+
+    const parentToConnectedComponents
+        = createMapParentToConnectedComponents(input, unionFind);
+
+    return calculateMaxAlternatingSum(parentToConnectedComponents);
+};
+
+function createMapParentToConnectedComponents(input: number[], unionFind: UnionFind): Map<number, ConnectedComponents> {
+    const parentToConnectedComponents = new Map();
+    for (let i = 0; i < input.length; ++i) {
+        const parent = unionFind.findParent(i);
+        if (!parentToConnectedComponents.has(parent)) {
+            parentToConnectedComponents.set(parent, new ConnectedComponents());
+        }
+        if (isEven(i)) {
+            ++parentToConnectedComponents.get(parent).numberOfEvenIndexes;
+        }
+        parentToConnectedComponents.get(parent).values.push(input[i]);
+    }
+
+    return parentToConnectedComponents;
+}
+
+function calculateMaxAlternatingSum(parentToConnectedComponents: Map<number, ConnectedComponents>): number {
+    let maxAlternatingSum = 0;
+    for (let current of parentToConnectedComponents.values()) {
+        current.values.sort((x, y) => y - x);
+        for (let i = 0; i < current.numberOfEvenIndexes; ++i) {
+            maxAlternatingSum += current.values[i];
+        }
+        for (let i = current.numberOfEvenIndexes; i < current.values.length; ++i) {
+            maxAlternatingSum -= current.values[i];
+        }
+    }
+    return maxAlternatingSum;
+}
+
+function isEven(value: number): boolean {
+    return value % 2 === 0;
+}
+
+class ConnectedComponents {
+
+    numberOfEvenIndexes = 0;
+    values = new Array();
+}
+
+class UnionFind {
+
+    parent: number[];
+    rank: number[];
+
+    constructor(numberOfElements: number) {
+        this.parent = new Array(numberOfElements);
+        this.rank = new Array(numberOfElements);
+        for (let i = 0; i < numberOfElements; ++i) {
+            this.parent[i] = i;
+            this.rank[i] = 1;
+        }
+    }
+
+    findParent(index: number): number {
+        if (this.parent[index] !== index) {
+            this.parent[index] = this.findParent(this.parent[index]);
+        }
+        return this.parent[index];
+    }
+
+    joinByRank(indexOne: number, indexTwo: number): void {
+        const first = this.findParent(indexOne);
+        const second = this.findParent(indexTwo);
+        if (first === second) {
+            return;
+        }
+
+        if (this.rank[first] >= this.rank[second]) {
+            this.parent[second] = first;
+            this.rank[first] += this.rank[second];
+        } else {
+            this.parent[first] = second;
+            this.rank[second] += this.rank[first];
+        }
+    }
+}
